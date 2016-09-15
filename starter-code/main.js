2625370c204bf39b3a600847c9517e3232ce7ce6
Jane Maguire <janemaguire@Janes-MacBook-Air.local>
2016-09-15 14:02:28 +0100

    Unit 10 added for loop and function

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,10 +3,18 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert ('You found a match!')
-} else if (cardThree === cardFour) {
-	alert ('You found a match!')
-} else {
-	alert (Sorry, try again.)
-}
+//if (cardOne === cardTwo) {
+//	alert ('You found a match!')
+//} else if (cardThree === cardFour) {
+//	alert ('You found a match!')
+//} else {
+//	alert (Sorry, try again.)
+//}
+
+var createBoard = function(x) {
+  for (var i = 1; num < 5; i += 1) {
+  var newDiv = document.createElement('div');
+  div.className = 'card';
+  world.appendChild(board);
+};
+newDiv.addEventListener('click', createBoard)
